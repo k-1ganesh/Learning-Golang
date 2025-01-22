f44ba5ce72c499ba2c1809c2b09f06c7f1c1f7ff
Ganesh <kaganesh12@gmail.com>
2025-01-22 14:41:06 +0530

    Understood the datatypes of Go

---
+++ Datatype/datatype.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+var p = 15
+func main() {
+    
+	var num int = 10
+	var check bool = true
+	var nm float32 = 23.4
+	var name string = "Ganesh"
+	fmt.Println(num  * 2)
+	fmt.Println(check)
+	fmt.Println(nm)
+	fmt.Print(name)
+	numm := 15
+	fmt.Println(numm)
+	fmt.Println(p) 
+}
