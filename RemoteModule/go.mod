150f585488ea0eeb1610673e029a713fa628bafb
Ganesh <kaganesh12@gmail.com>
2025-01-29 15:14:30 +0530

    Learned How to import remote module

---
+++ RemoteModule/go.mod
@@ -0,0 +1,5 @@
+module temp
+
+go 1.23.5
+
+require github.com/k-1ganesh/utility v0.0.0-20250129081316-108bde128467
---
+++ RemoteModule/main.go
@@ -0,0 +1,9 @@
+package main 
+import (
+	"fmt"
+	"github.com/k-1ganesh/utility"
+)
+
+func main() {
+	fmt.Println(utility.Square(5))
+}
